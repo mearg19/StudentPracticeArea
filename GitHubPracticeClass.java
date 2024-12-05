11daf3775a235b522cb6b44b3b3514ce36759f5b
mearg19 <magistudy1@gmail.com>
2024-12-05 11:09:48 -0800

    Greetings from mearg

--- GitHubPracticeClass.java
+++ GitHubPracticeClass.java
@@ -23,4 +23,7 @@ public class GitHubPracticeClass {
       public void Kbrom_Mehari(){
         System.out.println("I am Kbrom Mehari");
     }
+    public void Mearg_Abrha(){
+      System.out.println("Greetings from mearg");
+  }
 }
