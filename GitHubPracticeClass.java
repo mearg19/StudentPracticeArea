dc7be6e04b084abfc8d1bc809c1b04179be9c7d1
mearg19 <magistudy1@gmail.com>
2024-12-08 19:38:59 -0800

    Added a method to display greeting

--- GitHubPracticeClass.java
+++ GitHubPracticeClass.java
@@ -23,4 +23,7 @@ public class GitHubPracticeClass {
       public void Kbrom_Mehari(){
         System.out.println("I am Kbrom Mehari");
     }
+    public void MeargAbrha(){
+      System.out.println("greetings from Mearg");
+  }
 }
